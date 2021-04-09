5fbe6e1c13e9ba48025ded7098b653f309edd4cd
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2021-04-09 00:38:12 -0300

    - bootstrap and jquery packages - navbar and tasks components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
-
+import { Task } from './tasks/shared/task.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'task-manager';
+  task: Task = new Task(21, 'testing');
 }
