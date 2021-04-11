f7f8dde7e8bf3dbece93fe461ad08718706390dd
Rodrigo Toledo <rodrigo@rtoledo.inf.br>
2021-04-11 09:11:15 -0300

    - implement task observable, dont use services - must refact in future to use separate class

--- src/app/tasks/shared/task.observable.ts
+++ src/app/tasks/shared/task.observable.ts
@@ -1,20 +1,30 @@
-import { Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Task } from "./task.model";
+import { Injectable, OnInit } from '@angular/core';
+import { Observable, of } from "rxjs";
+import { Task } from './task.model';
 
-const TASKS: Array<Task> = [
+const getTasks: Observable<Task[]> = of([
   { id: 1, title: 'teste' },
   { id: 2, title: 'teste2' },
   { id: 3, title: 'teste4' }
-]
+]);
 
 @Injectable()
 
-export class TaskObservable{
-  public getTasks(): Observable<Task[]>{
-    let observable = Observable.create((observer) => {
-      observer.next(TASKS);
-    }).subscribe();
-    return observable;
+export class TaskObservable implements OnInit {
+  tasks: Observable<Task[]>
+
+  ngOnInit(){
+    // let observable = Observable.create((observer) => {
+    //   observer.next(TASKS);
+    // }).subscribe();
+    // this.tasks = observable;
+    this.tasks = getTasks;
+    // this.tasks.subscribe();
   }
+  // public getTasks(): Observable<Task[]>{
+  //   let observable = Observable.create((observer) => {
+  //     observer.next(TASKS);
+  //   }).subscribe();
+  //   return observable;
+  // }
 };
--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 import { faInfo, faAngry } from '@fortawesome/free-solid-svg-icons';
 import { Task } from './shared/task.model';
-import { TaskObservable } from './shared/task.observable';
-import { Observable } from 'rxjs';
+// import { TaskObservable } from './shared/task.observable';
+import { Observable, of } from 'rxjs';
+
+const getTasks: Observable<Task[]> = of([
+  { id: 1, title: 'teste' },
+  { id: 2, title: 'teste2' },
+  { id: 3, title: 'teste4' }
+]);
+
 
 @Component({
   selector: 'tasks',
   templateUrl: 'tasks.component.html',
-  providers: [ TaskObservable ]
+  providers: [  ]
+  // providers: [ TaskObservable ]
 })
 
 export class TasksComponent implements OnInit {
@@ -15,14 +23,12 @@ export class TasksComponent implements OnInit {
   public selectedTask: Task;
   public faInfo = faInfo;
   public faAngry = faAngry;
-  private taskObservable: TaskObservable;
+  // private taskObservable: TaskObservable;
 
-  constructor(taskObservable: TaskObservable) {
-    this.taskObservable = taskObservable;
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.tasks = this.taskObservable.getTasks();
+    this.tasks = getTasks;
   }
 
   public onSelect(task: Task):void {
